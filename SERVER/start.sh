baeffeea085ffe6e834474080d78e9ca88195115
gilangvperdana <gilangvirga271@gmail.com>
2021-04-14 07:14:31 +0000

    MC Server 1.16.5 Ubuntu 20.04

---
+++ SERVER/start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+while true
+do
+java -Xms8G -Xmx8G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar spigot-1.16.5.jar nogui
+echo "restarting in 10"
+sleep 10
+done
